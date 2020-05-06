ca16c3a5f8cedafb6f9765ec8ffed076324d6de8
Syndicate555 <saziz028@uottawa.ca>
2020-05-06 19:51:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash() {
@@ -13,8 +14,18 @@ class Block {
       this.index +
         this.previousHash +
         this.timestamp +
-        JSON.stringify(this.data)
+        JSON.stringify(this.data) +
+        this.nonce
     ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
   }
 }
 
