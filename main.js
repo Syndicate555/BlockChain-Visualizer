9feb60ffc79c3d2e637392c501471eef80ef4aa5
Syndicate555 <saziz028@uottawa.ca>
2020-05-07 02:33:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
 const { Blockchain, Transaction } = require("./BlockChain");
-
+const myKey = ec.keyFromPrivate(
+  "f018e59de21483e96365a93e1a42e8b7d1d00afb89934fde17e98d13c9c9ab77"
+);
 let syndiCoin = new Blockchain();
 
 syndiCoin.createTransaction(new Transaction("address1", "address2", 100));
