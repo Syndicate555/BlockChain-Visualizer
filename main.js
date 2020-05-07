fcee0f05960809821425a6a4e5da9c939873e012
Syndicate555 <saziz028@uottawa.ca>
2020-05-07 02:35:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const { Blockchain, Transaction } = require("./BlockChain");
 const myKey = ec.keyFromPrivate(
   "f018e59de21483e96365a93e1a42e8b7d1d00afb89934fde17e98d13c9c9ab77"
 );
+const myWalletAddress = myKey.getPublic("hex");
 let syndiCoin = new Blockchain();
+
+const tx1 = new Transaction(myWalletAddress, "public key goes here", 10);
 
 syndiCoin.createTransaction(new Transaction("address1", "address2", 100));
 syndiCoin.createTransaction(new Transaction("address2", "address1", 50));
