26f3f905be260d2af480bf28538b1ea72a18803d
Syndicate555 <saziz028@uottawa.ca>
2020-05-07 02:25:41 -0400

    Update BlockChain.js

--- BlockChain.js
+++ BlockChain.js
@@ -59,6 +59,14 @@ class Block {
     }
     console.log("Block mined: " + this.hash);
   }
+
+  hasValidTransactions() {
+    for (const tx of this.transactions) {
+      if (!tx.isValid()) {
+        return false;
+      }
+    }
+  }
 }
 
 class Blockchain {
