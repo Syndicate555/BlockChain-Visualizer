24209cf5180ba040269d176dd8318fac2ababbbd
Syndicate555 <saziz028@uottawa.ca>
2020-05-07 02:38:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,19 +11,8 @@ const tx1 = new Transaction(myWalletAddress, "public key goes here", 10);
 tx1.signTransaction(myKey);
 syndiCoin.addTransaction(tx1);
 
-syndiCoin.createTransaction(new Transaction("address1", "address2", 100));
-syndiCoin.createTransaction(new Transaction("address2", "address1", 50));
-
 console.log("\n Starting the miner.......");
-syndiCoin.mindPendingTransactions("saffats-address");
-console.log(
-  "\nBalance of Saffat is: ",
-  syndiCoin.getBalanceOfAddress("saffats-address")
-);
-
-console.log("\n Starting the miner.......");
-syndiCoin.mindPendingTransactions("saffats-address");
-
+syndiCoin.mindPendingTransactions(myWalletAddress);
 console.log(
   "\nBalance of Saffat is: ",
   syndiCoin.getBalanceOfAddress("saffats-address")
